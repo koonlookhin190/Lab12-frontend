1f3073ad887e1e2ad187bd17509841a4516a5cc1
koonlookhin <krit_tipnuan@cmu.ac.th>
2022-10-13 00:25:36 +0700

    2.3

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -29,7 +29,7 @@
         label="Select an Organizer"
       />
       <h3>The image of the Event</h3>
-      <UploadImages />
+      <UploadImages @changed="handleImages" />
 
       <button type="submit">Submit</button>
     </form>
@@ -75,6 +75,9 @@ export default {
         .catch(() => {
           this.$router.push('NetworkError')
         })
+    },
+    handleImages(files) {
+      console.log(files)
     }
   }
 }
